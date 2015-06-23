36606504f0fc55ea37004513daf241dbad010294
Marios Daoutis <mds@monads.io>
2015-06-23 16:31:05 +0200

    adding ps.top

--- core/ps.fish
+++ core/ps.fish
@@ -5,5 +5,14 @@ function ps
 end
 
 function ps.top
-  call $ps -eo pcpu,pid,user,args | sed 1d | sort -r -k 1 | head -10
+  switch $OS
+    case darwin
+      call $ps -eo pcpu,pid,user,args | sed 1d | sort -r -k 1 | head -10
+    case linux
+      call $ps -eo pcpu,pid,user,args | sed 1d | sort -r -k 1 | head -10
+    case syno
+      call $ps ; echo "fix this"
+    case '*'
+      out.ln "not supported"
+  end
 end
