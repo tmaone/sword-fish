c63c2f8e1f55e65be7d6d045ff8b73ec1402cc0f
Marios Daoutis <marios.daoutis@monolith.ai>
2015-06-25 12:15:23 +0200

    adding homebrew functions

--- plugins/brew/src/brew.fish
+++ plugins/brew/src/brew.fish
@@ -2,6 +2,32 @@ function brew
     /usr/local/bin/brew $argv
 end
 
+function brew.update
+  brew  update
+  brew.outdated
+  brew upgrade --all
+end
+
+function brew.outdated
+  brew outdated
+end
+
+function brew.doctor
+  brew  doctor
+end
+
 function brew.list
   brew list 2> /dev/null | awk '{ORS=" " ; print;}'
+end
+
+function brew.clean
+  brew cleanup;
+end
+
+function brew.pin
+  brew pin $argv;
+end
+
+function brew.unpin
+  brew pin $argv;
 end
