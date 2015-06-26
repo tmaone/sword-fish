a76b0fb9c785e8d18c3ca6ce950ef079881166ba
Marios Daoutis <marios.daoutis@monolith.ai>
2015-06-26 17:32:41 +0200

    multi-os

--- core/osx.fish
+++ core/osx.fish
@@ -1,14 +1,12 @@
 function osx --description 'Evaluates the command if the current OS is MacOS'
-    if set -q OS
-        if test $OS = "darwin"
-            if arg $argv
-                eval $argv
-            end
-            return 0
-        else
-            return 1
-        end
+
+    if test (os) = darwin
+      if arg $argv
+        eval $argv
+      end
+      return 0
     else
-        return 1
+      return 1
     end
+
 end
--- core/syno.fish
+++ core/syno.fish
@@ -1,14 +1,12 @@
 function syno --description 'Evaluates the command if the current Operating System is Synology'
-    if set -q OS
-        if test $OS = "syno"
-            if arg $argv
-                eval $argv
-            end
-            return 0
-        else
-            return 1
-        end
-    else
-        return 1
+
+  if test (os) = syno
+    if arg $argv
+      eval $argv
     end
+    return 0
+  else
+    return 1
+  end
+
 end
