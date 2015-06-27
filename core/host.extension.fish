2d85bc68ee6a84457bb7673032ff45a55ae1e4a2
Marios Daoutis <marios.daoutis@monolith.ai>
2015-06-27 18:18:02 +0200

    testing commit

--- core/host.extension.fish
+++ core/host.extension.fish
@@ -3,13 +3,13 @@ function host.extension
         if string.contains $argv[1] '.'
             echo $argv[1] | awk -F '.' '{print $NF}'
         else
-            debug "Host $argv[1] does not have an extenstion in it's hostname"
+            out " "
         end
     else
         if string.contains (host.name) '.'
             echo (host.name) | awk -F '.' '{print $NF}'
         else
-            debug "Local host does not have extenstion in it's hostname"
+            out " "
         end
     end
 end
