3d449286508c9bc9e11d056a2274b6344c8eb19b
Marios Daoutis <marios.daoutis@monolith.ai>
2015-06-28 23:00:11 +0200

    removed pwd's from sword.versions

--- core/sword.version.fish
+++ core/sword.version.fish
@@ -1,9 +1,6 @@
 function sword.version
   if git.is $sword_root
-    pwd.save
-    cd $sword_root
-    set -l sword_version (git describe --tags --always)
+    set -l sword_version (call $git --git-dir="$sword_root/.git" --work-tree="$sword_root" describe --tags --always)
     out $sword_version
-    pwd.cd
   end
 end
--- core/sword.version.git.fish
+++ core/sword.version.git.fish
@@ -1,9 +1,6 @@
 function sword.version.git
         if git.is $sword_root
-            pwd.save
-            cd $sword_root
-            set -l sword_version_git (git rev-parse --short HEAD)
+            set -l sword_version_git (call $git --git-dir="$sword_root/.git" --work-tree="$sword_root" rev-parse --short HEAD)
             out $sword_version_git
-            pwd.cd
         end
 end
