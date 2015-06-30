c5936f2a5bf5fbe0333ac50811b9bdf6be1a7d3a
Marios Daoutis <marios.daoutis@monolith.ai>
2015-06-30 16:05:23 +0200

    file.list

--- core/file.list.fish
+++ core/file.list.fish
@@ -1,3 +1,28 @@
 function file.list
-  # not yet
+
+  if arg.one $argv
+    if dir.exists $argv[1]
+      call $find $argv[1] -maxdepth 1 -type f -exec basename \{\} \;
+      return 0
+    else
+      call $find (pwd) -maxdepth 1 -type f -name "$argv[1]" -exec basename \{\} \;
+      return 0
+    end
+  end
+
+  if arg.two $argv
+    if dir.exists $argv[1]
+      call $find $argv[1] -maxdepth 1 -type f -name "$argv[2]" -exec basename \{\} \;
+      return 0
+    end
+    if dir.exists $argv[2]
+      call $find $argv[2] -maxdepth 1 -type f -name "$argv[1]" -exec basename \{\} \;
+      return 0
+    end
+    error "error"
+    return 1
+  end
+
+  call $find (pwd) -maxdepth 1 -type f -exec basename \{\} \;
+  return 0
 end
