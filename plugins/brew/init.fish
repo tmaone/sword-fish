5945a1330dadf31e18cc8af216b4d4380a17fec2
Marios Daoutis <marios.daoutis@monolith.ai>
2015-07-22 22:00:29 +0300

    brew init

--- plugins/brew/init.fish
+++ plugins/brew/init.fish
@@ -1,2 +1,18 @@
-osx find.program brew
+function brew.init.osx
+
+find.program brew
+
+set brew_path "/usr/local/bin" "/usr/local/sbin"
+
+for path in $brew_path
+  if dir.exists $path
+    if not path.contains $path
+      path.add $path
+    end
+  end
+end
+
+end
+
+osx brew.init.osx
 linux emit not_supported
