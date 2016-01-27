fb9113a7fb4126df1dd13a464e3d7ba5639db76d
Marios Daoutis <marios.daoutis@monolith.ai>
2016-01-28 00:12:13 +0100

    julia plugin

--- core/os.update.fish
+++ core/os.update.fish
@@ -1,9 +1,27 @@
 function os.update
-  sudo.validate
+  
+  sudo.validate;
+  
   osx.update;
+  
+  sudo.validate
+
   brew.update; brew.upgrade;
+
+  sudo.validate
+  
   cd ~/src/language/julia; git pull; make install; cd ~
+  
+  sudo.validate
+
   julia -e "Pkg.update();Pkg.build()"
+
+  sudo.validate
+
   apm update --confirm false
+
+  sudo.validate
+  
   sudo tlmgr update --self --all --reinstall-forcibly-removed
+
 end
