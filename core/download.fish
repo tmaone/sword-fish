17fffe05796e403a108ecf23b32646bf49c199ec
Marios Daoutis <marios.daoutis@monolith.ai>
2016-02-02 03:50:06 +0100

    download youtubedl

--- core/download.fish
+++ core/download.fish
@@ -9,3 +9,12 @@ end
 function download.web
 	wget --random-wait -r -p -e robots=off -U mozilla $argv[1]
 end
+
+function download.youtube
+
+end
+
+function download.youtube.audio
+	youtube-dl -x --audio-format mp3 --audio-quality 0 $argv[1]
+end
+
